be580341d3779c9a3f5053b98e8929cfcc091aa7
LinhChi2511 <chidao2511@gmail.com>
2024-08-16 17:51:39 +0700

    sửa lại file lỗi

--- js-co-ban/doi-tien-te.js
+++ js-co-ban/doi-tien-te.js
@@ -1,14 +1,16 @@
 function doitien(){
     let tien = parseFloat(document.getElementById('tien').value);
-    let vao = document.getElementById('vao').value
-    let ra = document.getElementById('ra').value
+    let vao = document.getElementById('vao').value;
+    let ra = document.getElementById('ra').value;
     let result
     if (vao == "USD" && ra == "VND"){
         result = tien*23000;
-        document.getElementById('kq').innerHTML = result + 'đ';
+        document.getElementById('kq').innerHTML = result + ' đ';
+        console.log(result);
     } else if (vao == "VND" && ra == "USD"){
         result = tien/23000
-        document.getElementById('kq').innerHTML = result + '$';
+        document.getElementById('kq').innerHTML = result + ' $';
+        console.log(result);
     }
     else {
         result = 'vui lòng chọn 2 giá trị khác nhau'
